d104b9b2c4d3df49785c90cd6e1d968758ddef31
Mats Lillas <mats.lillas@edu.vora.fi>
2021-10-23 00:05:39 +0300

    0.1.1

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-
-from re import search
+from PIL import Image
+import os
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mats.lillas\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-im = Image.open('images/test.png')
-text = pytesseract.image_to_string(im)
-text = text.lower()
+def rename():
+    os.walk('images')
+    imgDir = os.listdir()
+    imgDirLength = len(os.listdir())
+    ix = 0
+    for imgDir[ix] in range(imgDirLength):
+        os.rename(str(imgDir[ix]), 'image'+str(ix)+'.png')
+    os.walk('../')
 
-list = text.split(" ")
+rename()
 
-i = 0
-for i in range(len(list)):
-    if(list[i].find("total") != -1):
-        total = list[i]
+num = 0
+for num in range(len(os.listdir('images'))):
+    im = Image.open('images/image'+str(num)+'.png')
+    text = pytesseract.image_to_string(im)
 
-try:
-    total = total.replace("total", "")
-    total = total.replace("€", "")
-    total = total.replace("$", "")
-    total = total.replace(",", ".")
-    total = total.replace("\n", "")
-except:
-    print("epic fail")
-print(float(total))
+    text = text.lower()
+    list = text.split(" ")
+
+    i = 0
+    for i in range(len(list)):
+        if(list[i].find("total") != -1):
+            total = list[i]
+
+    try:
+        total = total.replace("total", "")
+        total = total.replace("€", "")
+        total = total.replace("$", "")
+        total = total.replace(",", ".")
+        total = total.replace("\n", "")
+    except:
+        print("epic fail")
+    print(float(total))
