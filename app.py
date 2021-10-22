fc1218032d956ad3cf0847af12b956228d5e6cab
Mats Lillas <mats.lillas@edu.vora.fi>
2021-10-22 12:32:08 +0300

    Stable Total variable

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ try:
 except ImportError:
     import Image
 
+from re import search
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mats.lillas\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 im = Image.open('images/test.png')
 text = pytesseract.image_to_string(im)
+text = text.lower()
 
-print(text)
+list = text.split(" ")
+
+i = 0
+for i in range(len(list)):
+    if(list[i].find("total") != -1):
+        print(list[i])
