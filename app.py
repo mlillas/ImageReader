ad6e7bb3d9a9e19ec462b93fa9d93650eb0b423c
Mats Lillas <mats.lillas@edu.vora.fi>
2021-10-23 18:17:31 +0300

    0.2

--- app.py
+++ app.py
@@ -3,20 +3,22 @@ import os
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mats.lillas\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
+folder = r'images\\'
+totalSum = 0.0
+
 def rename():
-    os.walk('images')
-    imgDir = os.listdir()
-    imgDirLength = len(os.listdir())
-    ix = 0
-    for imgDir[ix] in range(imgDirLength):
-        os.rename(str(imgDir[ix]), 'image'+str(ix)+'.png')
-    os.walk('../')
+    count = 0
+    for file_name in os.listdir(folder):
+        source = folder + file_name
+        destination = folder + str(count) + '.png'
+        os.rename(source, destination)
+        count+=1
 
 rename()
 
 num = 0
 for num in range(len(os.listdir('images'))):
-    im = Image.open('images/image'+str(num)+'.png')
+    im = Image.open(folder+str(num)+'.png')
     text = pytesseract.image_to_string(im)
 
     text = text.lower()
@@ -36,3 +38,6 @@ for num in range(len(os.listdir('images'))):
     except:
         print("epic fail")
     print(float(total))
+    totalSum += float(total)
+
+print('The total sum of your spendings is: '+str(round(totalSum, 2)))
